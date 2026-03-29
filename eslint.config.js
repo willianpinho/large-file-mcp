0f7493d07718d2607b16e1b1800f8d9e277abe8d
Willian Pinho <willianpinho@gmail.com>
2026-03-29 15:37:12 +0000

    refactor(lint): migrate ESLint from v8 to v9 with flat config
    Replace deprecated .eslintrc.json with eslint.config.js using
    typescript-eslint flat config. Update lint script to remove --ext flag.

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" },
+      ],
+      "no-console": "off",
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/", "docs/", "coverage/"],
+  }
+);
